210d7acf50166f63899a1d3e133cd6d80e6cf104
aneeshbharadwajka <aneesh.bharadwaj7@gmail.com>
2024-05-05 08:10:09 +0530

    update bootstrap script and remove software from brewfile that are broken

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,8 +19,12 @@ fi
 
 
 # Check for Oh My Zsh and install if not present
+# Add custom plugins for zsh
 if test ! $(which omz); then
   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k 
 fi
 
 # Update Homebrew recipes
@@ -28,4 +32,5 @@ brew update
 
 # Install all dependencies with bundle (See Brewfile)
 brew tap homebrew/bundle
+
 brew bundle --file ./system/Brewfile
